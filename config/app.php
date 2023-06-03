f72bb45c630c5c7127d23987190574054f076f17
saifhalila <saifhalila@gmail.com>
2023-06-03 17:32:39 +0100

    refactor: remove nitmedia package

--- config/app.php
+++ config/app.php
@@ -82,7 +82,7 @@ return [
 
     'locale' => 'en',
 
-     /*
+    /*
     |--------------------------------------------------------------------------
     | Application Locale Direction Configuration
     |--------------------------------------------------------------------------
@@ -191,7 +191,6 @@ return [
         App\Providers\BladeServiceProvider::class,
         App\Providers\HtmlMacroServiceProvider::class,
         App\Providers\HelpersServiceProvider::class,
-        Nitmedia\Wkhtml2pdf\L5Wkhtml2pdfServiceProvider::class,
 
     ],
 
@@ -246,7 +245,7 @@ return [
 
         // Attendize Class Alias
         'Markdown'     => GrahamCampbell\Markdown\Facades\Markdown::class,
-        'PDF'          => Nitmedia\Wkhtml2pdf\Facades\Wkhtml2pdf::class,
+        'PDF'          => Dompdf\Dompdf::class,
         'Utils'        => App\Attendize\Utils::class,
         'Excel'        => Maatwebsite\Excel\Facades\Excel::class,
     ],
